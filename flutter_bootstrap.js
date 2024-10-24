7117e0695ca7688164461417927f1ea5b543e20e
김성일 <qwedx1915@naver.com>
2024-10-24 21:43:27 +0900

    hot fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2905095608"
+    serviceWorkerVersion: "919410551"
   }
 });
